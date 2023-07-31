118eb2e766a7494a24d833a5295b3366673c3cb7
Hayata Suenaga <hayata@expensify.com>
2023-07-31 11:03:17 -0700

    chore(eslint): update configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-    extends: 'expensify',
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.native.js', '.web.js'],
-            },
-        },
+    extends: [
+        'eslint:recommended',
+        'airbnb',
+        'plugin:react/recommended',
+        'jsx-a11y',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'airbnb-typescript',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
     },
+    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
+    root: true,
 };
