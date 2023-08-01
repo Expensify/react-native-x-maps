d3a72110491b8f320d247d0d6e862d47a6424205
Hayata Suenaga <hayata@expensify.com>
2023-08-01 16:14:51 -0700

    chore(eslint): add rules from expensify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,47 @@ module.exports = {
         'plugin:jsx-a11y/recommended',
         'prettier',
     ],
+    rules: {
+        'arrow-parens': [
+            'error',
+            'as-needed',
+            {
+                requireForBlockBody: true,
+            },
+        ],
+        'no-invalid-this': 'error',
+        'react/function-component-definition': [
+            'error',
+            {
+                namedComponents: 'function-declaration',
+                unnamedComponents: 'arrow-function',
+            },
+        ],
+        'no-restricted-syntax': [
+            'error',
+            // The following four selectors prevent the usage of async/await
+            {
+                selector: 'AwaitExpression',
+                message: 'async/await is not allowed',
+            },
+            {
+                selector: 'FunctionDeclaration[async=true]',
+                message: 'async functions are not allowed',
+            },
+            {
+                selector: 'FunctionExpression[async=true]',
+                message: 'async functions are not allowed',
+            },
+            {
+                selector: 'ArrowFunctionExpression[async=true]',
+                message: 'async functions are not allowed',
+            },
+            {
+                selector: 'MethodDefinition[async=true]',
+                message: 'async methods are not allowed',
+            },
+        ],
+    },
     overrides: [
         {
             files: ['*.ts', '*.tsx'],
