13d2a1614f5c9895d327ffbf1e4c06cf995b6ec0
Hayata Suenaga <hayata@expensify.com>
2023-08-01 16:33:41 -0700

    chore: add example code

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+function ExampleComponent() {
+    return (
+        <View>
+            <Text>Hello, mobile</Text>
+        </View>
+    );
+}
+
+export default ExampleComponent;
---
+++ src/index.web.tsx
@@ -0,0 +1,9 @@
+function ExampleComponent() {
+    return (
+        <div>
+            <p>Hello, web</p>
+        </div>
+    );
+}
+
+export default ExampleComponent;
