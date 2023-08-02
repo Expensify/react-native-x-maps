fad3ad0189b0ab5e65ba6b11930d21468d2c9314
Hayata Suenaga <hayata@expensify.com>
2023-08-02 14:22:36 -0700

    style: run prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,7 @@ module.exports = {
     root: true,
     ignorePatterns: ['node_modules', 'dist'],
     plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'airbnb',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:jsx-a11y/recommended',
-        'prettier',
-    ],
+    extends: ['eslint:recommended', 'airbnb', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:jsx-a11y/recommended', 'prettier'],
     rules: {
         'arrow-parens': [
             'error',
@@ -55,11 +48,7 @@ module.exports = {
         {
             files: ['*.ts', '*.tsx'],
             plugins: ['@typescript-eslint'],
-            extends: [
-                'plugin:@typescript-eslint/recommended-type-checked',
-                'airbnb-typescript',
-                'prettier',
-            ],
+            extends: ['plugin:@typescript-eslint/recommended-type-checked', 'airbnb-typescript', 'prettier'],
             parser: '@typescript-eslint/parser',
             parserOptions: {
                 project: './tsconfig.json',
