6858c216841122d83342981242a62cd4879da936
Hayata Suenaga <hayata@expensify.com>
2023-08-11 12:36:43 -0700

    style: use default export

--- src/MapView/utils.ts
+++ src/MapView/utils.ts
@@ -1,7 +1,4 @@
-// ESLint rule forces the usage of default export if there is only one export.
-// But not using default export here as more util functions might be added later.
-// eslint-disable-next-line import/prefer-default-export
-export const getBounds = (waypoints: Array<[number, number]>): {southWest: [number, number]; northEast: [number, number]} => {
+function getBounds(waypoints: Array<[number, number]>): {southWest: [number, number]; northEast: [number, number]} {
     const lngs = waypoints.map((waypoint) => waypoint[0]);
     const lats = waypoints.map((waypoint) => waypoint[1]);
 
@@ -9,4 +6,8 @@ export const getBounds = (waypoints: Array<[number, number]>): {southWest: [numb
         southWest: [Math.min(...lngs), Math.min(...lats)],
         northEast: [Math.max(...lngs), Math.max(...lats)],
     };
+}
+
+export default {
+    getBounds,
 };
